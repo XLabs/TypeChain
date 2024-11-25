ee4d0fde2f8d8027bfd32d5a8b184d1b2a62768d
Sebastián Claudio Nale <sebinale@gmail.com>
2024-11-25 19:34:28 -0300

    WIP in memory files API.

--- packages/typechain/src/typechain/runTypeChain.ts
+++ packages/typechain/src/typechain/runTypeChain.ts
@@ -7,7 +7,7 @@ import { debug } from '../utils/debug'
 import { detectInputsRoot } from '../utils/files'
 import { findTarget } from './findTarget'
 import { loadFileDescriptions, processOutput, skipEmptyAbis } from './io'
-import { CodegenConfig, Config, PublicConfig, Services } from './types'
+import { CodegenConfig, Config, FileDescription, PublicConfig, Services } from './types'
 
 interface Result {
   filesGenerated: number
@@ -46,6 +46,51 @@ export async function runTypeChain(publicConfig: PublicConfig): Promise<Result> 
   const target = findTarget(config)
 
   const fileDescriptions = loadFileDescriptions(services, config.filesToProcess)
+
+  debug('Executing beforeRun()')
+  filesGenerated += processOutput(services, config, await target.beforeRun())
+
+  debug('Executing beforeRun()')
+  for (const fd of fileDescriptions) {
+    debug(`Processing ${relative(config.cwd, fd.path)}`)
+
+    filesGenerated += processOutput(services, config, await target.transformFile(fd))
+  }
+
+  debug('Running afterRun()')
+  filesGenerated += processOutput(services, config, await target.afterRun())
+
+  return {
+    filesGenerated,
+  }
+}
+
+export async function runTypeChainInMemory(publicConfig: Omit<PublicConfig, "filesToProcess">, fileDescriptions: FileDescription[]): Promise<Result> {
+  const allFiles = skipEmptyAbis(publicConfig.allFiles)
+  if (allFiles.length === 0) {
+    return {
+      filesGenerated: 0,
+    }
+  }
+
+  // skip empty paths
+  const config: Config = {
+    flags: DEFAULT_FLAGS,
+    inputDir: detectInputsRoot(allFiles),
+    ...publicConfig,
+    allFiles,
+    filesToProcess: [],
+  }
+  const services: Services = {
+    fs,
+    prettier,
+    mkdirp,
+  }
+  let filesGenerated = 0
+
+  const target = findTarget(config)
+
+  // const fileDescriptions = loadFileDescriptions(services, config.filesToProcess)
 
   debug('Executing beforeRun()')
   filesGenerated += processOutput(services, config, await target.beforeRun())
